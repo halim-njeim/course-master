6e033ad1d02731a0fc613abb76f26b6dcbb13422
CaptainSlash44 <michaelnjeim44@gmail.com>
2025-04-28 10:44:45 +0300

    added traits folder and response traits file

---
+++ backend/app/Traits/response.traits.js
@@ -0,0 +1,29 @@
+export const successResponse = (res, data, code = 200) => {
+  const responseData = typeof data === "string" ? { message: data } : data;
+
+  return res.status(code).json({
+    status: "success",
+    ...responseData,
+  });
+};
+
+export const errorResponse = (res, error, code = 500) => {
+  const errorMessage =
+    error instanceof Error
+      ? error.message
+      : typeof error === "string"
+      ? error
+      : "An error occured";
+
+  return res.status(code).json({
+    status: "error",
+    message: errorMessage,
+  });
+};
+
+export const notFoundResponse = (res, message = "Resource not found") => {
+  return res.status(404).json({
+    status: "error",
+    message,
+  });
+};
